01c3f8d9d28771efd42be436fbb9f8ffa5f47e74
Michael Koch <mk-neuko@outlook.com>
2022-10-30 13:11:31 +0100

    safe the current work.

--- PerformanceTestDetermineWinnerTicTacToe/GameBoardProvider.cs
+++ PerformanceTestDetermineWinnerTicTacToe/GameBoardProvider.cs
@@ -2,5 +2,29 @@
 
 public class GameBoardProvider
 {
+    private Dictionary<string, GameBoard> gameBoardList;
+    private GameBoard gameBoard;
 
+    public GameBoardProvider()
+    {
+            
+    }
+
+    public void CreateGameBoards(bool isXInitialToken)
+    {
+        var curentGameBoard = new GameBoard();
+    }
+}
+
+public class GameBoard
+{
+    private List<GameBoardArea> gameBoardAreaList;
+    private bool isXWinner;
+    private bool isOWinner;
+    private bool isTie;
+
+    public GameBoard()
+    {
+        
+    }
 }
